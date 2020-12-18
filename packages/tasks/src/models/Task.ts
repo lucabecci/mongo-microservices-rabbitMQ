35c54b0a617384e2f0e910d2c985945c15ddf9e4
luca <beccibrian@gmail.com>
2020-12-18 14:16:12 -0300

    feat(@micro/tasks): controller and router for task added

--- packages/tasks/src/models/Task.ts
+++ packages/tasks/src/models/Task.ts
@@ -1,25 +1,25 @@
-import {Schema, model, Document, SchemaDefinition} from 'mongoose'
+import { Schema, model, Document, SchemaDefinition } from "mongoose";
 
 interface ITask extends Document {
-    title: string
-    descriptiton: string
-    priority: number
+  title: string;
+  descriptiton: string;
+  priority: number;
 }
 
 class TaskSchema {
-    private _schemaDefiniton: SchemaDefinition
-    public _taskSchema: Schema
+  private _schemaDefiniton: SchemaDefinition;
+  public _taskSchema: Schema;
 
-    constructor(){
-        this._schemaDefiniton = {
-            title: {type: String, required: true, length: 20},
-            description: {type: String, required: true, length: 80},
-            priority: {type: Number, required: true, default: 3}
-        }
-        this._taskSchema = new Schema(this._schemaDefiniton, { timestamps: true })
-    }
+  constructor() {
+    this._schemaDefiniton = {
+      title: { type: String, required: true, length: 20 },
+      description: { type: String, required: true, length: 80 },
+      priority: { type: Number, required: true, default: 3 },
+    };
+    this._taskSchema = new Schema(this._schemaDefiniton, { timestamps: true });
+  }
 }
 
-const taskSchema = new TaskSchema;
+const taskSchema = new TaskSchema();
 
-export default model<ITask>("Task", taskSchema._taskSchema)
+export default model<ITask>("Task", taskSchema._taskSchema);
